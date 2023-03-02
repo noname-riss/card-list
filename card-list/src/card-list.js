6afb98ea024fb9195f2f4d01041c7b3dd3661d7a
noname-riss <122493434+noname-riss@users.noreply.github.com>
2023-03-02 11:20:15 -0500

    Adding CSS

--- card-list/src/card-list.js
+++ card-list/src/card-list.js
@@ -10,46 +10,16 @@ class CardList extends LitElement {
   }
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--card-list-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
+    university-class-card{
+    border: 5px solid black;
+    background-color: blue;
+    display: inline-flex;
+    margin: 82px auto 32px;
+    padding: 0 16px;
+    max-width: 400px;
+    flex-direction: column;
+    align-items: center;
+  }
   `;
 
   constructor() {
